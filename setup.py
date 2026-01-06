98b7cc26f913725049e491b1cb55672542f59035
p4prince2 <p4prince2@gmail.com>
2026-01-07 01:32:37 +0530

    cookiescutter initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='these is the testing of the gradion and cookies cutter',
+    author='Prince',
+    license='',
+)
